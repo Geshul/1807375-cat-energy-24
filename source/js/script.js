a14b4defc48ee0eb84f18efa92a95ac352c8f270
George <gufrar@gmail.com>
2022-02-01 19:17:48 +0300

    допуск правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,12 @@
-var navMain = document.querySelector(".main-navigation");
-var navToggle = document.querySelector(".main-navigation__toggle");
+const navMain = document.querySelector(".main-navigation");
+const navToggle = document.querySelector(".main-navigation__toggle");
 
-
-  navToggle.addEventListener("click", function() {
-    if (navMain.classList.contains("main-navigation--closed")) {
-      navMain.classList.remove("main-navigation--closed");
-      navMain.classList.add("main-navigation--opened");
-    } else {
-      navMain.classList.add("main-navigation--closed");
-      navMain.classList.remove("main-navigation--opened");
-    }
-  });
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-navigation--closed")) {
+    navMain.classList.remove("main-navigation--closed");
+    navMain.classList.add("main-navigation--opened");
+  } else {
+    navMain.classList.add("main-navigation--closed");
+    navMain.classList.remove("main-navigation--opened");
+  }
+});
